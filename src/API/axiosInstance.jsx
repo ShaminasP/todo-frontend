7aceeee18a3bfa06f1b2929a084a6e9e5ec83e16
ShaminasP <mrzeddem@gmail.com>
2023-04-13 18:11:04 +0530

    backend url changed

--- src/API/axiosInstance.jsx
+++ src/API/axiosInstance.jsx
@@ -1,5 +1,5 @@
 import Axios from "axios"
 export const AxiosUser = Axios.create({
-    baseURL: "http://localhost:3031",
+    baseURL: "https://todo-backend-h63u.onrender.com",
     headers: { 'Content-Type': 'application/json' }
 })
